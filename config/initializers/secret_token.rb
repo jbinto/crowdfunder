2e0fc6e711dd2e5cd26bb30fce4ba75396e9c9b5
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-09 08:25:33 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdfunderInClass::Application.config.secret_token = 'ba20772bb7b7fcae87daea75a6bd2cf006183ba2cbd953f1bdd1b7cd3f35aa7a6a6b888a00861ecd59ac80f31a20755c377626bb39e9f1de2ed20b780c54f1ea'
