87cbe3229e100a0ed9bd49f8bfd1f75d710b78ef
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-09 10:27:59 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Project.destroy_all
+
+user = User.create!(full_name: "John Doe", email: "johndoe@example.org", password: "abc123")
+
+50.times do |i|
+  project = user.projects.create!(title: "Project #{i}", teaser: "Teaser text #{i}", 
+    description: "description #{i}", goal: 13000)
+end
