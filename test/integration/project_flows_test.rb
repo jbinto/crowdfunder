089107375820300f68d16ea51ac7933f0346e120
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-09 11:09:55 -0400

    Use factory_girl to improve the integration test

--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 
 class ProjectFlowsTest < ActionDispatch::IntegrationTest
   test "the index page lists all our projects" do
+    FactoryGirl.create(:project, :title => "one")
+    FactoryGirl.create(:project, :title => "two")
+    FactoryGirl.create(:project, :title => "three")
+
     visit '/projects'
     assert page.has_content?('Listing projects')
+    assert page.has_content?('one')
+    assert page.has_content?('two')
+    assert page.has_content?('three')
   end
 end
+  
