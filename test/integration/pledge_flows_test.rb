c5c333e3a2fa3fe67aba2e4765099b61f1e614ff
Jesse Buchanan <jbinto@jbinto.ca>
2013-04-11 13:55:04 -0400

    Add integration test for successful pledge

--- test/integration/pledge_flows_test.rb
+++ test/integration/pledge_flows_test.rb
@@ -2,6 +2,31 @@ require 'test_helper'
 
 class PledgeFlowsTest < ActionDispatch::IntegrationTest
   test "unauthenticated users backing a project are asked to sign in" do    
+    project = goto_project
+    
+    # Try to back the project without being signed in
+    click_link 'Back this project'
+    assert_equal sign_in_path, current_path
+  end
+
+  test "authenticated user can make a pledge" do
+    setup_logged_in_user
+    project = goto_project
+
+    # Back the project    
+    click_link 'Back this project'
+    assert_equal new_project_pledge_path(project), current_path
+
+    # Make a pledge
+    fill_in "pledge[amount]", :with => 500
+    click_button "Make your pledge"
+
+    # User should be redirected back to root with a message
+    assert_equal root_path, current_path
+    assert find('.alert:first').has_content?("Thanks for pledging")
+  end
+
+  def goto_project
     # Make a single dummy project
     project = FactoryGirl.create(:project)
 
@@ -9,11 +34,8 @@ class PledgeFlowsTest < ActionDispatch::IntegrationTest
     visit '/projects'
     click_link 'Wifi enabled shoes'
     assert_equal project_path(project), current_path
-    
-    # Try to back the project without being signed in
-    click_link 'Back this project'
-    assert_equal sign_in_path, current_path
 
+    project
   end
 
 end
